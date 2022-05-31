b9319b3b2785324e0bdfa5395b21eac983a807b4
vishnu244 <vishnunali244@gmail.com>
2022-05-31 12:52:53 +0530

    [Vishnu]-Calculating Fare per Ride

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Cab Invoice Generator!");
+using CabInvoiceGenerator;
+
+Console.WriteLine("Welcome to Cab Invoice Generator");
+
+InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+double Fare = invoiceGenerator.TotalFare(10, 5);
+Console.WriteLine("Total Fare for Journey = " + Fare);
+
+    
